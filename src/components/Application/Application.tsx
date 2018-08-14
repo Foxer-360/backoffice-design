e7179231ec5d5336420b0f02a4e5a62c340356db
Filip Suchý <nevim42@gmail.com>
2018-08-14 22:24:40 +0200

    Prepared for our structure
    Removed example files and created our simple structure prepared for development. Also ejected React scripts, added SASS and Ant Design.

---
+++ src/components/Application/Application.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+const { Component } = React;
+
+class Application extends Component<{}, {}> {
+
+  public render() {
+    return (
+      <div>
+        <span>Simple react application with Ant Design to prepare static graphics for BackOffice application.</span>
+      </div>
+    );
+  }
+
+}
+
+export default Application;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+
+import Application from '@source/components/Application';
+import registerServiceWorker from '@source/services/serviceWorker';
+
+import '@source/styles/main.scss';
 
 ReactDOM.render(
-  <App />,
+  <Application />,
   document.getElementById('root') as HTMLElement
 );
+
 registerServiceWorker();
